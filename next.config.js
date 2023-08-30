392ae32bd44d6f6e8a75d052fc412a1447ba2603
iwishyoujoy <dasha@skv.name>
2023-08-31 00:32:24 +0300

    feat(configuration): changes next config to get images from remote servers

---
+++ next.config.js
@@ -0,0 +1,20 @@
+// module.exports = {
+//     images: {
+//       remotePatterns: [
+//         {
+//           protocol: 'https',
+//           hostname: 'www.freetogame.com',
+//           port: '',
+//           pathname: '/g/*',
+//         },
+//       ],
+//     },
+//   }
+
+module.exports = {
+    images: {
+      domains: ['www.freetogame.com'],
+      loader: 'default',
+    },
+  };
+  

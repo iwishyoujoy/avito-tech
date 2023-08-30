90d1d37be21611d81e8c84c005eb4a4b294c5667
iwishyoujoy <dasha@skv.name>
2023-08-30 15:28:02 +0300

    feat(utils): added commitlint + its config

---
+++ .cz-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    types: [
+      { value: 'docs', name: 'docs:\t\tDocumentation only changes' },
+      { value: 'feat', name: 'feat:\t\tA new feature' },
+      { value: 'fix', name: 'fix:\t\tA bug fix' },
+      { value: 'refactor', name: 'refactor:\tA code change that neither fixes a bug nor adds a feature' },
+      { value: 'revert', name: 'revert:\tRevert some commits' },
+      { value: 'style', name: 'style:\tChanges that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)' },
+      { value: 'test', name: 'test:\tAdd missing tests or correcting existing tests' },
+    ],
+  
+    scopes: [
+        { "name": "api" },
+        { "name": "component" },
+        { "name": "configuration" },
+        { "name": "pages" },
+        { "name": "redux" },
+        { "name": "stylesheet" },
+        { "name": "utils" },
+        { "name": "other" }
+    ]
+  };
+  

0ed1ae711a4cde119d831f606303cc4624ada479
iwishyoujoy <dasha@skv.name>
2023-08-31 18:57:56 +0300

    style(configuration): changed config for commitlint

--- .cz-config.js
+++ .cz-config.js
@@ -6,7 +6,7 @@ module.exports = {
       { value: 'refactor', name: 'refactor:\tA code change that neither fixes a bug nor adds a feature' },
       { value: 'revert', name: 'revert:\tRevert some commits' },
       { value: 'style', name: 'style:\tChanges that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)' },
-      { value: 'test', name: 'test:\tAdd missing tests or correcting existing tests' },
+      { value: 'test', name: 'test:\t\tAdd missing tests or correcting existing tests' },
     ],
   
     scopes: [
